6b415edecc195d7454e6a8bd130a8a8add495865
Daniel Mundt <900458+dmundt@users.noreply.github.com>
2021-03-26 23:44:26 +0100

    Fix home position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1230,11 +1230,11 @@
 #define Y_BED_SIZE 150
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -2
+#define Y_MIN_POS -6
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE - X_MIN_POS
+#define Y_MAX_POS Y_BED_SIZE - Y_MIN_POS
 #define Z_MAX_POS 100
 
 /**
